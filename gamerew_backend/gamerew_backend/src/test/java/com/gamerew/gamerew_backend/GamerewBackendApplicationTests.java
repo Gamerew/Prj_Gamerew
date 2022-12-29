e9f246b7b77225eded15f3dbf7547846e1b4509d
KutayOzkoc <kutayozkoc99@gmail.com>
2022-12-30 01:12:46 +0300

    gr_backend user base implementation added

---
+++ gamerew_backend/gamerew_backend/src/test/java/com/gamerew/gamerew_backend/GamerewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamerew.gamerew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerewBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
